a9dcc0012353e00080dee505944746f3ca612060
Atefe <aa.asgariyan12@gmail.com>
2025-01-15 09:16:50 +0330

    use Image component of the next

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
 
 import "@/app/_styles/globals.css";
+import Header from "@/app/_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The wild oasis",
@@ -9,19 +14,20 @@ export const metadata = {
     template: "%s | The Wild Oasis",
     default: "Welcome | The wild Oasis",
   },
-  description: "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-900 text-primary-50 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by wild oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-900 text-primary-50 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 ">
+          <main className="max-w-7xl mx-auto ">{children}</main>
+        </div>
       </body>
     </html>
   );
