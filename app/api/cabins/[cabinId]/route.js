a3ce00e7eb7104b8d58042447d10af195eb9fb30
Atefe <aa.asgariyan12@gmail.com>
2025-04-04 00:24:08 +0330

    create API endpoint

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,13 @@
+import { getCabin } from "@/app/_lib/data-service";
+
+export async function GET(request, { params }) {
+  const { cabinId } = params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([getCabin(cabinId)]);
+
+    return Response.json({ cabin, bookedDates });
+  } catch {
+    return Response.json({ message: "Cabin not found." });
+  }
+}
---
+++ app/api/route.js
@@ -0,0 +1,5 @@
+export async function GET() {
+    return Response.json({test:"test"})
+}
+export async function POST() {}
+export async function DELETE() {}
