869aecff4f544b4b23e58dc5fed47803f96a94f0
Atefe <aa.asgariyan12@gmail.com>
2025-04-03 20:18:23 +0330

    Sharing states between client and server components

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -2,13 +2,25 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   // noStore();
   const cabins = await getCabins();
   if (!cabins.length) return null;
+
+  let displayedCabins;
+  if (filter === "all") displayedCabins = cabins;
+  if (filter === "small")
+    displayedCabins = cabins.filter((cabin) => cabin.max_capacity <= 3);
+  if (filter === "medium")
+    displayedCabins = cabins.filter(
+      (cabin) => cabin.max_capacity >= 4 && cabin.max_capacity <= 7
+    );
+  if (filter === "large")
+    displayedCabins = cabins.filter((cabin) => cabin.max_capacity >= 8);
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
